91a0591919bed63998da828275494733bfc76880
Sharada Prasanna Mohanty <spmohanty91@gmail.com>
2025-04-07 23:38:19 +0200

    Update gitignore and add examples to user_config.py

--- agents/user_config.py
+++ agents/user_config.py
@@ -1,3 +1,11 @@
+from agents.dial_agent import DummyResponseAgent
 from agents.test_agent import TestResponseAgent
+from agents.openai_agent import OpenAIAgent
 
-UserAgent = TestResponseAgent
+
+UserAgent = TestResponseAgent 
+# UserAgent = DummyResponseAgent
+# UserAgent = OpenAIAgent # Uncomment this to test the OpenAI agent (requires API key)
+
+
+
